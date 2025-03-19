802f5a4973a72ae094161476d0c51b225e87af65
pasalkarvaibhavi <vaibhavipasalkar2004@gamil.com>
2025-03-19 23:58:16 +0530

    Modified

--- SEM-2/REACT/rpractical/src/App.js
+++ SEM-2/REACT/rpractical/src/App.js
@@ -4,6 +4,7 @@ import HelloWorld from './Component/HelloWorld';
 import Counter from './Component/Counter';
 import Form from './Component/Form';
 import DataFetcher from './Component/DataFetcher';
+import ProgressBar from './Component/ProgressBar';
 
 function App() {
   return (
@@ -12,7 +13,15 @@ function App() {
     <Counter/>
     <Form/>
     <DataFetcher/>
-  
+
+    <div className="App">
+      <h1>Progress Bar with Percentage</h1>
+      <ProgressBar progress={30} color="#ff6f61" height="25px"  showLabel={true}/>
+      <ProgressBar progress={60} color="#76c7c0" height="25px" showLabel={true}/> 
+      <ProgressBar progress={50} color="#ffa500" height="25px" showLabel={true}/> 
+      <ProgressBar progress={85} color="#4caf50" height="25px" showLabel={true}/> 
+      <ProgressBar progress={95} color="#6a11cb" height="25px" showLabel={true}/> 
+    </div>
    
 
    </div>
